e44af5d13936c4e8e9e6e898161203566f9e25eb
Lucas PenzeyMoog <lucaspenzeymoog@gmail.com>
2018-08-31 15:13:14 -0500

    added 1.9 string_rotation

---
+++ cracking_the_code_interview/chapter_1/1.9_string_rotation/lib/string_rotation.rb
@@ -0,0 +1,7 @@
+#assume you have a method isSubstring which
+#checks if one wors is a substring of another
+#Given two strings, s1 and s2, write code to 
+#check if s2 is a rotation of s1 using only one 
+#call to isSubstring (ef 'waterbottle' is a 
+#rotation of "erbottlewat")
+
